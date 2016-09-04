de2de36226e24552b0850c435935ecb030816a32
hectorcoronado <coronado.hector@gmail.com>
2016-09-03 17:06:57 -0700

    creating first component

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,25 @@
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message.'
+    };
+  },
+  render: function () {
+    var name = this.props.name;
+    var message = this.props.message;
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
+      </div>
+    );
+  }
+});
+
+var firstName = "Brenda";
+
 ReactDOM.render(
-  <h1>Hello React!</h1>,
+  <Greeter name={ firstName }/>,
   document.getElementById('app')
 );
