14ea4d7db7e70325fd84dc1b6df4c353858e7bd4
hectorcoronado <coronado.hector@gmail.com>
2016-09-05 20:49:29 -0700

    components are working and message is set by state

--- public/app.jsx
+++ public/app.jsx
@@ -5,7 +5,7 @@ var GreeterMessage = React.createClass({
     return (
       <div>
         <h1>Hello {name}!</h1>
-        <p>Some {message}???</p>
+        <p>{message}</p>
       </div>
     )
   }
@@ -15,18 +15,34 @@ var GreeterForm = React.createClass({
   onFormSubmit: function(e) {
     e.preventDefault();
 
+    var updates = {};
     var name = this.refs.name.value;
+    var message = this.refs.message.value;
 
     if (name.length > 0) {
       this.refs.name.value = "";
-      this.props.onNewName(name);
+      updates.name = name;
     }
+
+    if (message.length > 0) {
+      this.refs.message.value = "";
+      updates.message = message;
+    }
+
+    this.props.onNewData(updates);
   },
   render: function() {
     return (
       <form onSubmit={this.onFormSubmit}>
-        <input type="text" ref="name"/>
-        <button>Set Name</button>
+        <div>
+          <input type="text" ref="name" placeholder="Enter Name"/>
+        </div>
+        <div>
+          <textarea ref="message" placeholder="Enter Message"></textarea>
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
       </form>
     )
   }
@@ -42,23 +58,22 @@ var Greeter = React.createClass({
 
   getInitialState: function() {
     return {
-      name: this.props.name
+      name: this.props.name,
+      message: this.props.message
     };
   },
 
-  handleNewName: function(name) {
-    this.setState({
-      name: name
-    });
+  handleNewData: function(updates) {
+    this.setState(updates);
   },
 
   render: function () {
     var name = this.state.name;
-    var message = this.props.message;
+    var message = this.state.message;
     return (
       <div>
         <GreeterMessage name={name} message={message}/>
-        <GreeterForm onNewName={this.handleNewName}/>
+        <GreeterForm onNewData={this.handleNewData}/>
       </div>
     );
   }
