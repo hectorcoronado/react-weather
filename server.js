09bc4a08dfc0b23878fded43a3ba7e9f70b63a59
hectorcoronado <coronado.hector@gmail.com>
2016-09-08 18:39:27 -0700

    Prep for Heroku deployment

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log('Express server is running on http://localhost:3000/');
+app.listen(PORT, function() {
+  console.log('Express server is running on ' + port);
 });
